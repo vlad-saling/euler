43d3730d0bb63c29f06a4f33c0fde288b376ece6
vlad saling <vladislav.saling@protonmail.com>
2019-07-24 16:15:51 +0200

    haskell solution

---
+++ euler1.hs
@@ -0,0 +1,5 @@
+module Main where
+    import Data.List (union)
+    main = putStrLn (show ans)
+    -- ans =  sum [ x | x <- [0..999], (mod x 3) == 0 || (mod x 5) == 0] source & credit: https://github.com/nayuki/Project-Euler-solutions/blob/master/haskell/p001.hs
+    ans = sum (union [3,6..999] [5,10..999])
